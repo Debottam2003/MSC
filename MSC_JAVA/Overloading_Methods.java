92ddea350a37972cd176101bbf0bd2de3ab45a83
unknown <debottamkar2003@gmail.com>
2024-10-27 13:42:20 +0530

    again pc commit

--- MSC_JAVA/Overloading_Methods.java
+++ MSC_JAVA/Overloading_Methods.java
@@ -1,3 +1,21 @@
+//Method Overloading function name is same but the function  signature is different = (function name + no of argument / type of arguments)
 public class Overloading_Methods {
-    
+    public static void main(String args[]) {
+        Overloading_Methods func = new Overloading_Methods();
+        func.function(10, 5);
+        func.function(10, 5, 20);
+        func.function(10.5, 5.6);
+    }
+
+    public void function(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public void function(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+
+    public void function(double a, double b) {
+        System.out.println(a + b);
+    }
 }
