7349c5c2cb34e7cb9f634193ee36d9a7156c0f6a
unknown <debottamkar2003@gmail.com>
2024-12-08 15:41:48 +0530

    pc

--- MSC_JAVA/Thread/Mt2.java
+++ MSC_JAVA/Thread/Mt2.java
@@ -2,7 +2,7 @@ public class Mt2 implements Runnable{
     @Override
     public void run() {
         while (true) {
-            System.out.println("thread 2...");
+            System.out.println("                thread 2...");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
