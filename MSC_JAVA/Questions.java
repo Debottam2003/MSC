ba8336b4d3e6c048c4bcfc14421c4eac97ecd2a3
Debottam Kar <debottamkar2003@.com>
2025-02-11 21:49:00 +0530

    increment decrement

---
+++ MSC_JAVA/Questions.java
@@ -0,0 +1,48 @@
+public class Questions {
+    public static void main(String[] args) {
+
+        //Module 1
+        /*
+         * double a = 0.0, b = 0.0, c = 0.0, d = 0.0, e = 0.0, r = 0.0, s = 0.0, y = 0.0;
+         * double res = 0.0;
+         * res = Math.sqrt((a * a * a) + (b * b * b) + (c * c * c));
+         * res = 3 - r * Math.pow(s, (2 * r)) + 4 * s;
+         * res = Math.abs(Math.pow(e , y) - y);
+         * res = a + b / Math.pow((c + d), 3);
+         */
+
+        /*
+         * 2.
+         * int x = 11;
+         * (i) System.out.println(10 * ++x - 1); // ans: 119
+         * (ii) System.out.println(++x + x++); // 24
+         */
+
+        /*
+         * 3.
+         * int x = 2;
+         * x += x++ + ++x / 2;
+         * System.out.println(x);// ans: 6
+         */
+
+        /*
+         * 4.
+         * int x = 5;
+         * (i) System.out.println(++x * 2); // ans: 12
+         * (ii) System.out.println(x++ * 2); // ans: 10
+         */
+
+        /*
+         * 5.
+         * int y = 8, x = 13;
+         * int c = 0;
+         * c = ++y + x + ++y;
+         * System.out.println(c); // ans: 32
+         */
+
+        // Module 2
+        // 1. You are over weight // as weight = 70
+        // 2. BetterBest // as s = 4
+        // 3. a // as a < b as 10 < 20
+    }
+}
