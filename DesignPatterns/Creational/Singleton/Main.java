79d184501dea0386de2126662c446ae68f6ab696
Debottam Kar <debottamkar2003@gmail.com>
2025-03-01 14:31:42 +0530

    adding ooad notes

--- DesignPatterns/Creational/Singleton/Main.java
+++ DesignPatterns/Creational/Singleton/Main.java
@@ -2,7 +2,9 @@ package DesignPatterns.Creational.Singleton;
 
 public class Main {
     public static void main(String[] args) {
-        Singleton instance = Singleton.createInstance("postgres", "loscahost", "okudera2003", 5000);
+        Singleton instance = Singleton.createInstance("postgres", "localhost", "okudera2003", 5000);
         System.out.println(instance);
+        Singleton instance2 = Singleton.createInstance("mysql", "localhost", "ichinose", 3306);
+        System.out.println(instance2);
     }
 }
