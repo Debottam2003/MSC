e9c58c2126da7c6a8d538f6fcbfbf38f6bbb2e21
Debottam Kar <debottamkar2003@gmail.com>
2025-03-15 22:38:38 +0530

    in built ds in java using util package

---
+++ MSC_JAVA/ds/ds1.java
@@ -0,0 +1,85 @@
+package ds;
+
+import java.util.*;
+
+public class ds1 {
+    public static void main(String[] args) {
+        // int[] arr = new int[10];
+        // ArrayList<String> todo = new ArrayList<>();
+        // todo.add("debottam kar");
+        // todo.add("react");
+        // todo.add("js");
+        // for(String str : todo){
+        // System.out.println(str);
+        // }
+        // todo.add("go");
+        // todo.add("rust");
+        // for(String str : todo){
+        // System.out.println(str);
+        // }
+        // todo.remove(3);
+        // System.out.println(arr.length);
+        // System.out.println(todo.size());
+        // for(String str : todo){
+        // System.out.println(str);
+        // }
+        // Stack<Integer> s = new Stack<>();
+        // s.push(10);
+        // s.push(20);
+        // s.push(30);
+        // System.out.println(s);
+        // System.out.println("Top element is: " + s.peek());
+        // System.out.println("Stack size is: " + s.size());
+        // System.out.println("Popped element is: " + s.pop());
+        // System.out.println("Top element after pop is: " + s.peek());
+        // System.out.println("Stack size after pop is: " + s.size());
+        // Queue<Integer> queue = new LinkedList<>();
+        // queue.offer(10);
+        // queue.offer(20);
+        // queue.offer(30);
+        // System.out.println(queue);
+        // System.out.println(queue.poll());
+        // System.out.println(queue);
+        // queue.offer(40);
+        // System.out.println(queue);
+        // LinkedList<Object> linked = new LinkedList<>();
+        // linked.add(2);
+        // linked.add("debottam kar");
+        // linked.add(s);
+        // System.out.println(linked);
+        // linked.addFirst(queue);
+        // linked.forEach(System.out::println);
+        // linked.addLast("go");
+        // System.out.println(linked);
+        // System.out.println(linked.size());
+        // LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
+        // lhs.add(10);
+        // lhs.add(20);
+        // System.out.println(lhs);
+        // lhs.addFirst(20);
+        // System.out.println(lhs);
+        // HashMap<Integer, String> h = new HashMap<>();
+        // h.put(1, "Debottam");
+        // h.put(2, "Java");
+        // h.put(3, "Go");
+        // System.out.println(h); // Output: {1=Debottam, 2=Java, 3=Go}
+        // HashSet<String> hs = new HashSet<>();
+        // hs.add("Java");
+        // hs.add("Python");
+        // hs.add("Go");
+        // hs.add("Rust");
+        // System.out.println("HashSet: " + hs);
+        // TreeMap<Integer, String> tm = new TreeMap<>();
+        // tm.put(1, "js");
+        // tm.put(3, "java");
+        // tm.put(4, "go");
+        // tm.put(2, "rust");
+        // System.out.println(tm);
+        // TreeSet<String> ts = new TreeSet<>();
+        // ts.add("js");
+        // ts.add("java");
+        // ts.add("go");
+        // ts.add("rust");
+        // System.out.println(ts);
+    }
+}
