1ad31e351df4ff8df055abf401fc5f8bc0a74cbd
Debottam Kar <debottamkar2003@gmail.com>
2025-03-31 13:22:35 +0530

    lcs

--- DSA/lcs.c
+++ DSA/lcs.c
@@ -5,7 +5,7 @@ void lcs(char a[], char b[], int m,  int n)
     int l[m + 1][n + 1];
     for(int i = 0; i <= m; i++){
         for(int j = 0; j <= n; j++){
-            if(i ==0 || j == 0){
+            if(i == 0 || j == 0){
                 l[i][j] = 0;
             }
             else{
