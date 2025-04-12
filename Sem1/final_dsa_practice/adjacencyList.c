35c8ac75d43c5ea6fcce6190b13ea5b03a8036a6
Debottam Kar <debottamkar2003@gmail.com>
2025-04-12 22:39:15 +0530

    assignment

--- Sem1/final_dsa_practice/adjacencyList.c
+++ Sem1/final_dsa_practice/adjacencyList.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct node {
     int data;
+    int weight;
     struct node* next;
 };
-struct node* insert(struct node* head, int data){
+
+// Insert function updated to include weight
+struct node* insert(struct node* head, int data, int weight){
     struct node* newnode = (struct node*)malloc(sizeof(struct node));
     newnode->data = data;
+    newnode->weight = weight;
     newnode->next = NULL;
     if(head == NULL){
-        head = newnode;
-        return head;
+        return newnode;
     }
     struct node* temp = head;
     while(temp->next != NULL){
@@ -19,41 +23,79 @@ struct node* insert(struct node* head, int data){
     temp->next = newnode;
     return head;
 }
+
 int main(){
-    int v, v1, v2, e; 
+    int v, v1, v2, e, weight;
+    int graphType;
+
+    printf("Choose graph type:\n");
+    printf("1. Undirected Unweighted\n");
+    printf("2. Undirected Weighted\n");
+    printf("3. Directed Unweighted\n");
+    printf("4. Directed Weighted\n");
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &graphType);
+
     printf("Enter the number of vertices in the graph: ");
     scanf("%d", &v);
+
     struct node* adj[v];
-    for(int i = 0;i < v; i++){
+    for(int i = 0; i < v; i++){
         adj[i] = NULL;
     }
-    // for(int i = 0; i < v; i++){
-    //     printf("Enter the number of vertices connected with vertex %d: ",i);
-    //     scanf("%d", &e);
-    //     for(int j = 0; j < e; j++){
-    //         int data;
-    //         printf("Enter %d the vertex connected with vertex %d: ", j+1, i);
-    //         scanf("%d", &data);
-    //         adj[i] = insert(adj[i], data);
-    //     }
-    // }
+
     printf("Enter the number of edges in the graph: ");
     scanf("%d", &e);
+
     for(int i = 0; i < e; i++){
-        printf("Enter the edge %d: ", i + 1);
-        scanf("%d%d", &v1, &v2);
-        adj[v1] = insert(adj[v1], v2);
-        adj[v2] = insert(adj[v2], v1);
+        printf("Enter edge %d (format: v1 v2", i + 1);
+        if(graphType == 2 || graphType == 4){
+            printf(" weight");
+        }
+        printf("): ");
+        
+        if(graphType == 2 || graphType == 4){
+            scanf("%d%d%d", &v1, &v2, &weight);
+        } else {
+            scanf("%d%d", &v1, &v2);
+            weight = 0;  // default for unweighted
+        }
+
+        switch(graphType){
+            case 1: // Undirected Unweighted
+                adj[v1] = insert(adj[v1], v2, weight);
+                adj[v2] = insert(adj[v2], v1, weight);
+                break;
+            case 2: // Undirected Weighted
+                adj[v1] = insert(adj[v1], v2, weight);
+                adj[v2] = insert(adj[v2], v1, weight);
+                break;
+            case 3: // Directed Unweighted
+                adj[v1] = insert(adj[v1], v2, weight);
+                break;
+            case 4: // Directed Weighted
+                adj[v1] = insert(adj[v1], v2, weight);
+                break;
+            default:
+                printf("Invalid graph type selected.\n");
+                return 1;
+        }
     }
+
+    // Print adjacency list
     for(int i = 0; i < v; i++){
-        printf("\nAjacency list of vertex %d: \n", i);
+        printf("\nAdjacency list of vertex %d:\n", i);
         struct node* temp = adj[i];
         printf("%d -> ", i);
         while(temp != NULL){
-            printf("%2d -> ", temp->data);
+            if(graphType == 2 || graphType == 4)
+                printf("(%d, w:%d) -> ", temp->data, temp->weight);
+            else
+                printf("%d -> ", temp->data);
             temp = temp->next;
         }
         printf("NULL\n");
     }
+
     return 0;
 }
