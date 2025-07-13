0d55029903a8e4f52602fe2907017dfa5af202bb
Debottam Kar <debottamkar2003@gmail.com>
2025-07-13 14:24:08 +0530

    10th july dbms assignment part 1 done

--- Sem2/Adbms/master_data/app.js
+++ Sem2/Adbms/master_data/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
 import pkg from 'pg';
 const { Pool } = pkg;
 
@@ -7,17 +9,40 @@ const pool = new Pool({
     max: 100
 });
 
-let data = await pool.query("select id from items limit 1");
+let data = await pool.query("select item_id from items limit 1");
 
 // console.log(data.rows);
+let __filename = fileURLToPath(import.meta.url);
+let __dirname = path.dirname(__filename);
+// console.log(__filename);
+// console.log(__dirname);
 
 let app = express();
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/masterdata", (req, res) => {
-    res.send("<h1>This is a form</h1>");
+    res.sendFile(path.join(__dirname, 'masterdata.html'));
 });
+
+app.post("/saveMasterData", async (req, res) => {
+    try {
+        console.log(req.body);
+        let { item_id, name, item_description, price, type } = req.body;
+        if (!item_id || !name || !item_description || !price || !type) {
+            res.status(400).send("Submit all the fields");
+            return;
+        }
+        console.log(req.body);
+        let { rows } = await pool.query("insert into items(item_id, name, item_description, price, type) values($1, $2, $3, $4, $5) returning name", [item_id, name, item_description, price, type]);
+        console.log(rows);
+        return res.status(200).send("Item added successfully.");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal Server error");
+    }
+});
+
 
 app.listen(3333, () => {
     console.log("server is listening and servering on port:", 3333);
