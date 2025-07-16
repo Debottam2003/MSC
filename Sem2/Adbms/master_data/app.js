1db0b6d1afbdf7eeb1dbc7544a9249c61b85f907
Debottam Kar <debottamkar2003@gmail.com>
2025-07-17 00:19:35 +0530

    shifted to online db

--- Sem2/Adbms/master_data/app.js
+++ Sem2/Adbms/master_data/app.js
@@ -5,7 +5,8 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    connectionString: "postgres://postgres:[password]localhost:3000/restaurant",
+    // connectionString: "postgres://postgres:[password]localhost:3000/restaurant",
+    connectionString: "postgresql://postgres.klkpybbgsjpkgwurrxkw:okudera2003@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
     max: 100
 });
 
